43dd43f7ff89f5d6f40045540659907f1af13a19
Olha Valchak <o.valchak@do-it.co>
2022-03-10 17:43:15 +0200

    First commit.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardTest from './components/CardTest';
+
+function App() {
+  return (
+    <div className="App">
+      <CardTest />
+    </div>
+  );
+}
+
+export default App;
