369ba1c52782f74969aca3fe2a1caa3d1cab9cd5
Olha Valchak <o.valchak@do-it.co>
2022-03-11 13:09:36 +0200

    Add styling in page, CssBaseline.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { CardTest } from './components/CardTest';
-
+import CssBaseline from '@mui/material/CssBaseline';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <CardTest />
     </div>
   );
